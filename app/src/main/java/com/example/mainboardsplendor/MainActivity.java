dfa5c8e04f7aa7778799dd2e5ed7bd796fe78d82
Michael Christian <118243761+Lasciarmi@users.noreply.github.com>
2024-11-07 23:11:04 +0700

    coba

---
+++ app/src/main/java/com/example/mainboardsplendor/MainActivity.java
@@ -0,0 +1,273 @@
+package com.example.mainboardsplendor;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.LayoutInflaterCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mainboardsplendor.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        ActivityMainBinding binding =ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // Init nama player
+        binding.scoreBoardPlayer1.playerName.setText("MC");
+        binding.scoreBoardPlayer2.playerName.setText("Theo");
+
+        binding.scoreBoardPlayer1.totalPrivilegePlayer.setText("1");
+
+        // Init token board (TODO: IMPLEMENT SPIRAL TOKEN)
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                View view = LayoutInflater.from(this).inflate(R.layout.custom_token, binding.tokenBoard.splendorDuelBoard, false);
+                binding.tokenBoard.splendorDuelBoard.addView(view);
+            }
+        }
+
+        // Init Grid list_blue_token pada bag player
+        for (int i = 0; i< 4; i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.custom_token, binding.layoutPlayer1Bag.listBlueToken.listToken, false);
+            ImageView tokenView = view.findViewById(R.id.token_view);
+            CardView cardView = view.findViewById(R.id.cardView_token);
+
+            // Set Image token
+            tokenView.setImageResource(R.drawable.blue_token);
+            // Set bg CardView token
+            int color4blueToken = ContextCompat.getColor(this, R.color.color4blueToken);
+            cardView.setCardBackgroundColor(color4blueToken);
+            binding.layoutPlayer1Bag.listBlueToken.listToken.addView(view);
+        }
+
+        // Init Grid list_white_token pada bag player
+        for (int i = 0; i< 4; i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.custom_token, binding.layoutPlayer1Bag.listWhiteToken.listToken, false);
+            ImageView tokenView = view.findViewById(R.id.token_view);
+            CardView cardView = view.findViewById(R.id.cardView_token);
+
+            // Set Image token
+            tokenView.setImageResource(R.drawable.white_token);
+            // Set bg CardView token
+            int color4whiteToken = ContextCompat.getColor(this, R.color.white);
+            cardView.setCardBackgroundColor(color4whiteToken);
+            binding.layoutPlayer1Bag.listWhiteToken.listToken.addView(view);
+        }
+
+        // Init Grid list_green_token pada bag player
+        for (int i = 0; i< 4; i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.custom_token, binding.layoutPlayer1Bag.listGreenToken.listToken, false);
+            ImageView tokenView = view.findViewById(R.id.token_view);
+            CardView cardView = view.findViewById(R.id.cardView_token);
+
+            // Set Image token
+            tokenView.setImageResource(R.drawable.green_token);
+            // Set bg CardView token
+            int color4greenToken = ContextCompat.getColor(this, R.color.color4greenToken);
+            cardView.setCardBackgroundColor(color4greenToken);
+            binding.layoutPlayer1Bag.listGreenToken.listToken.addView(view);
+        }
+
+        // Init Grid list_black_token pada bag player
+        for (int i = 0; i< 4; i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.custom_token, binding.layoutPlayer1Bag.listBlackToken.listToken, false);
+            ImageView tokenView = view.findViewById(R.id.token_view);
+            CardView cardView = view.findViewById(R.id.cardView_token);
+
+            // Set Image token
+            tokenView.setImageResource(R.drawable.black_token);
+            // Set bg CardView token
+            int color4blackToken = ContextCompat.getColor(this, R.color.black);
+            cardView.setCardBackgroundColor(color4blackToken);
+            binding.layoutPlayer1Bag.listBlackToken.listToken.addView(view);
+        }
+
+        // Init Grid list_red_token pada bag player
+        for (int i = 0; i< 4; i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.custom_token, binding.layoutPlayer1Bag.listRedToken.listToken, false);
+            ImageView tokenView = view.findViewById(R.id.token_view);
+            CardView cardView = view.findViewById(R.id.cardView_token);
+
+            // Set Image token
+            tokenView.setImageResource(R.drawable.red_token);
+            // Set bg CardView token
+            int color4redToken = ContextCompat.getColor(this, R.color.color4redToken);
+            cardView.setCardBackgroundColor(color4redToken);
+            binding.layoutPlayer1Bag.listRedToken.listToken.addView(view);
+        }
+
+        for (int i = 0; i< 2; i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.custom_token, binding.layoutPlayer1Bag.listPearlToken.listToken, false);
+            ImageView tokenView = view.findViewById(R.id.token_view);
+            CardView cardView = view.findViewById(R.id.cardView_token);
+
+            // Set Image token
+            tokenView.setImageResource(R.drawable.pearl_token);
+            // Set bg CardView token
+            int color4pearlToken = ContextCompat.getColor(this, R.color.color4pearlToken);
+            cardView.setCardBackgroundColor(color4pearlToken);
+            binding.layoutPlayer1Bag.listPearlToken.listToken.addView(view);
+            // TODO making reverse layout for all list token
+        }
+
+        // INIT CARD_TOP
+        for (int i = 0; i < 3; i++){
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.setMargins(5, 0, 5, 0);
+
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(R.drawable.blank_card);
+            imageView.setLayoutParams(params);
+
+            binding.cardBoard.cardStoreTop.addView(imageView);
+        }
+
+        // INIT CARD_MID
+        for (int i = 0; i < 4; i++){
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.setMargins(5, 0, 5, 0);
+
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(R.drawable.blank_card);
+            imageView.setLayoutParams(params);
+
+            binding.cardBoard.cardStoreMid.addView(imageView);
+        }
+
+        // INIT CARD_BOT
+        for (int i = 0; i < 5; i++){
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.setMargins(5, 0, 5, 0);
+
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(R.drawable.blank_card);
+            imageView.setLayoutParams(params);
+
+            binding.cardBoard.cardStoreBot.addView(imageView);
+        }
+
+        // INIT RESERVED_CARD
+        for (int i = 0; i<4; i++){
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.setMargins(5, 20, 5, 0);
+
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(R.drawable.blank_card);
+            imageView.setLayoutParams(params);
+
+            binding.cardBoard.reservedCard.addView(imageView);
+        }
+
+        // INIT CARD_STACK
+        for (int i=0; i<5; i++){
+            ImageView card = new ImageView(this);
+            card.setImageResource(R.drawable.blank_card); // Ganti dengan Kartu Token
+            card.setLayoutParams(new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            ));
+            card.setTranslationY(i * 70); // Mengatur jarak Y antar Kartu
+
+            binding.layoutPlayer1Bag.blueCardStack.addView(card);
+        }
+
+        for (int i=0; i<5; i++){
+            ImageView card = new ImageView(this);
+            card.setImageResource(R.drawable.blank_card); // Ganti dengan Kartu Token
+            card.setLayoutParams(new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            ));
+            card.setTranslationY(i * 70); // Mengatur jarak Y antar Kartu
+
+            binding.layoutPlayer1Bag.whiteCardStack.addView(card);
+        }
+
+        for (int i=0; i<5; i++){
+            ImageView card = new ImageView(this);
+            card.setImageResource(R.drawable.blank_card); // Ganti dengan Kartu Token
+            card.setLayoutParams(new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            ));
+            card.setTranslationY(i * 70); // Mengatur jarak Y antar Kartu
+
+            binding.layoutPlayer1Bag.greenCardStack.addView(card);
+        }
+
+        for (int i=0; i<5; i++){
+            ImageView card = new ImageView(this);
+            card.setImageResource(R.drawable.blank_card); // Ganti dengan Kartu Token
+            card.setLayoutParams(new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            ));
+            card.setTranslationY(i * 70); // Mengatur jarak Y antar Kartu
+
+            binding.layoutPlayer1Bag.blackCardStack.addView(card);
+        }
+
+        for (int i = 0; i < 3; i++){
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.setMargins(5, 0, 5, 0);
+
+            ImageView card = new ImageView(this);
+            card.setImageResource(R.drawable.blank_card);
+            card.setLayoutParams(params);
+
+            binding.layoutPlayer1Bag.cardReservedPlayer.addView(card);
+        }
+
+        addNewCard(binding.layoutPlayer1Bag.redCardStack);
+        addNewCard(binding.layoutPlayer1Bag.redCardStack);
+        addNewCard(binding.layoutPlayer1Bag.redCardStack);
+        addNewCard(binding.layoutPlayer1Bag.redCardStack);
+
+
+        binding.layoutPlayer1Bag.playerNameBot.setText("MC");
+        binding.layoutPlayer2Bag.playerNameBot.setText("Theo");
+
+        binding.taskBar.taskBarTakeGems.setVisibility(View.INVISIBLE);
+        binding.taskBar.taskBarUsePrivilege.setVisibility(View.GONE);
+    }
+
+    // Method untuk nambah Card Stack
+    public void addNewCard(FrameLayout redCardStack) {
+        ImageView card = new ImageView(this);
+        card.setImageResource(R.drawable.blank_card);
+
+        int cardSpacing = 70; // Sesuaikan agar hanya sebagian atas yang terlihat
+
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        // Hitung jumlah kartu yang ada untuk mengatur posisi kartu baru
+        params.topMargin = redCardStack.getChildCount() * cardSpacing;
+        card.setLayoutParams(params);
+
+        // Tambahkan kartu ke dalam redCardStack
+        redCardStack.addView(card);
+    }
+}
